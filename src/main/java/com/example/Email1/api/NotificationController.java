ef6d5959922caf0d65933d624308472d935b9d2c
TTranchese <tommaso.tranchese.96@gmail.com>
2023-05-20 20:48:31 +0200

    changed imports

--- src/main/java/com/example/Email1/api/NotificationController.java
+++ src/main/java/com/example/Email1/api/NotificationController.java
@@ -7,7 +7,11 @@ import com.example.Email1.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
